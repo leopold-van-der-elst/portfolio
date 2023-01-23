d2cfc5cd2e3ec8909ba4900eadd912af9db0d169
Leopold van der Elst <leopoldvde@hotmail.com>
2023-01-23 15:53:39 +0100

    Upload new CV

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.getElementById("website1").addEventListener("click",function(){
 
 document.getElementById("contactBtn").addEventListener("click",function(){
     const link = document.createElement("a");
-    link.href = "/CV-2023.pdf";
+    link.href = "/CV-2k23_final.pdf";
     link.download = "Leopold-van-der-Elst-CV.pdf";
     link.click();
 })
